cf43f833384ade20d78cdf987aaefa17da97a454
Amirhossein Vejahat <amir.vejahat.av@gmail.com>
2020-01-23 22:42:24 +0330

    Create init.pp

---
+++ modules/motd/manifests/init.pp
@@ -0,0 +1,18 @@
+class motd {
+  
+  $boot_parition = $facts['partitions'].filter | $key, $value | {
+   $value['mount'] == '/boot'
+   }
+   
+  File { '/etc/motd',
+          path    =>  '/etc/motd',
+          owner   =>  'root',
+          group   =>  'root',
+          content =>  inline_template("<% Time.now %> BOOT Partition = ${boot_partition}")
+  
+  
+  }
+
+
+
+}

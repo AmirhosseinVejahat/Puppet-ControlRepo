f1be87feccf930e28e281395fbd73404f6524458
Amirhossein Vejahat <amir.vejahat.av@gmail.com>
2020-01-23 22:46:37 +0330

    Update init.pp

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -1,14 +1,14 @@
 class motd {
   
-  $boot_parition = $facts['partitions'].filter | $key, $value | {
-   $value['mount'] == '/boot'
+  $root_parition = $facts['partitions'].filter | $key, $value | {
+   $value['mount'] == '/'
    }
    
   File { '/etc/motd':
           path    =>  '/etc/motd',
           owner   =>  'root',
           group   =>  'root',
-          content =>  inline_template("<% Time.now %> BOOT Partition = ${boot_partition}")
+          content =>  inline_template("<% Time.now %> Root Partition = ${root_partition}")
   
   
   }

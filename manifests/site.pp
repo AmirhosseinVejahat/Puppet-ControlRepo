3a989254697f922a439c2b5946765a3daccb5b21
Amirhossein Vejahat <amir.vejahat.av@gmail.com>
2020-01-23 22:03:46 +0330

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,18 @@ node 'master.puppet.local' {
     include role::master
 }
 
+node 'agent1.puppet.local' {
+
+  user {"somerandomusername":
+        ensure => present,
+        managehome => true,
+        home => '/var/somerandomusername',
+        }
+
+
+
+}
+
 node default {
 
   include role::master
